2631e4bb41a97b6f024131966d504dfe786d0e44
bkm3342 <mandalrabindranath81@gmail.com>
2025-07-30 01:37:31 +0530

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -17,8 +17,8 @@ CONFIG_FILE = "info_channels.json"
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_url = "https://genprofile2.vercel.app/generate"
-        self.generate_url = "https://glob-info2.vercel.app/"
+        self.api_url = "https://glob-info2.vercel.app/info?uid="
+        self.generate_url = "https://genprofile2.vercel.app/generate?uid="
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
         self.cooldowns = {}
